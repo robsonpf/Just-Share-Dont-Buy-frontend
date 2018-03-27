5741166645aa03f08b2b90a6cca39871161b63bb
Sophie <sophie4ngo@gmail.com>
2018-03-26 23:42:21 -0700

    Add categories skeleton

--- reservations.js
+++ reservations.js
@@ -4,7 +4,7 @@
 // output a piece of DOM for each reservation
 
 
-$('#exampleModal').on('show.bs.modal', function (event) {
+$('#shareItemModal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var recipient = button.data('whatever') // Extract info from data-* attributes
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
