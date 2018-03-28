c7637c38d77285fcada299c46eb8585629a2f2de
Sophie <sophie4ngo@gmail.com>
2018-03-27 23:33:05 -0700

    Display all items on category page

--- categories.js
+++ categories.js
@@ -1,10 +1,23 @@
 let category = $.url('?category');
 $( document ).ready(() => {
-  axios.get('/categories/' + category)
+  axios.get(`/categories/${category}`)
     .then(response => {
       console.log(response);
       const category = response.data
       $('#category-title').html(category.name)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+  axios.get(`/categories/${category}/items`)
+    .then(response => {
+      console.log(response);
+      const items = response.data;
+      items.forEach(item => {
+        $('#items').append(`<p><b>${item.name}</b></p>`)
+        $('#items').append(`<p>${item.description}</p>`)
+      })
     })
     .catch(error => {
       console.log(error);
--- reservations.js
+++ reservations.js
@@ -3,27 +3,23 @@
 // loop through result
 // output a piece of DOM for each reservation
 
-const baseURL = 'http://localhost:3000/item'
-
-
 document.addEventListener('DOMContentLoaded', () => {
-  createItem()
-  
+  // createItem()
 })
 
-const createItem () => {
-  axios.post(`${baseURL}`, {
+const createItem = () => {
+  axios.post(`/item`, {
     "category_id":3,
     "user_id":3,
     "description":"Bla Bla Bla",
     "name":"My Item"
   })
-}.then(result => {
-  console.log(result);
-})
-.catch(error => {
-  console.log(error)
-})
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => {
+    console.log(error)
+  })
 }
 
 //
