a5063a9463b022a8079854896cb438a13b2bca43
Robson Farias <fariasrobson.r@gmail.com>
2018-03-28 11:25:42 -0700

    Fix merge conflicts

--- categories.js
+++ categories.js
@@ -10,3 +10,35 @@ $( document ).ready(() => {
       console.log(error);
     });
 });
+
+// const baseURL = 'http://localhost:3000'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   axios.all([
+//     axios.get(`${baseURL}/items`),
+//     axios.get(`${baseURL}/users`),
+//     axios.get(`${baseURL}/categories`)
+//   ])
+//   .then(axios.spread((items, users, categories) => {
+//     console.log(items.data);
+//     console.log(users.data);
+//     console.log(categories.data);
+//
+//
+//     let categoryItems = []
+//
+//     let categoryHeaderArray = document.querySelectorAll('.category')
+//     categoryHeaderArray.forEach(category => {
+//       console.log('CATEGORY === ', category.id);
+//
+//       categoryItems.push(items.data.filter(item => item.category_id === Number(category.id)))
+//       console.log(categoryItems);
+//
+//     })
+//   }))
+// })
+//
+//
+// loadData = () => {
+//
+// }
--- reservations.js
+++ reservations.js
@@ -2,42 +2,26 @@
 // make an ajax request with axios to get all reservations
 // loop through result
 // output a piece of DOM for each reservation
-
-const baseURL = 'http://localhost:3000/item'
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  createItem()
-  
-})
-
-const createItem () => {
-  axios.post(`${baseURL}`, {
-    "category_id":3,
-    "user_id":3,
-    "description":"Bla Bla Bla",
-    "name":"My Item"
-  })
-}.then(result => {
-  console.log(result);
-})
-.catch(error => {
-  console.log(error)
-})
-}
-
 //
-// function createItem() {
-//   $.post(`http://localhost:3000/item`, {
+// const baseURL = 'http://localhost:3000/item'
+//
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   createItem()
+//
+// })
+//
+// const createItem () => {
+//   axios.post(`${baseURL}`, {
 //     "category_id":3,
 //     "user_id":3,
 //     "description":"Bla Bla Bla",
 //     "name":"My Item"
 //   })
-//     .then(result => {
-//       console.log(result);
-//     })
-//     .catch(error => {
-//       console.log(error)
-//     })
+// }.then(result => {
+//   console.log(result);
+// })
+// .catch(error => {
+//   console.log(error)
+// })
 // }
