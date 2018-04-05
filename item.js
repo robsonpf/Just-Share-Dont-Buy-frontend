18324b72d37ea03093ef0b1ed3ccf8069cac7ecf
Sophie <sophie4ngo@gmail.com>
2018-04-04 20:18:48 -0700

    Add ability to unreserve items

--- item.js
+++ item.js
@@ -5,6 +5,31 @@ let item = $.url('?item');
 
 // When the page loads
 $(document).ready(() => {
+  // Setup up click listener for 'Book' button
+  $('#book').click(event => {
+    axios.patch(`${baseUrl}/items/${item}`, {reserved: true})
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  $('#unbook').click(event => {
+    axios.patch(`${baseUrl}/items/${item}`, {reserved: false})
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  $('#modal').on('hidden.bs.modal', () => {
+    location.reload();
+  });
+
   // Make a GET request for the item
   axios.get(`${baseUrl}/items/${item}`)
     .then(response => {
@@ -13,6 +38,13 @@ $(document).ready(() => {
       $('#item-description').html(item.description)
       $('#item-image').html(`<img src="${item.photo}" height=300 width=425 />`)
       $('#date').html(new Date(item.created_at));
+      if (item.reserved) {
+        $('#book').hide();
+        $('#modal-title').html(`Successfully unbooked ${item.name}!`)
+      } else {
+        $('#unbook').hide();
+        $('#modal-title').html(`Successfully reserved ${item.name}!`)
+      }
       $('#availability').html(item.reserved ? "Not Available" : "Available")
       axios.get(`${baseUrl}/users/${item.user_id}`)
         .then(response => {
@@ -27,16 +59,3 @@ $(document).ready(() => {
     });
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById("book").addEventListener('click', (event) => {
-    axios.patch(`${baseUrl}/items/${item}`, {
-      "reserved": true
-    })
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-  })
-})
