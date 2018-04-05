fa10bda35193be289d0032a1221c9c7f670af2e9
Robson Farias <fariasrobson.r@gmail.com>
2018-04-04 23:55:29 -0700

    Fixing item.js file

--- item.js
+++ item.js
@@ -1,21 +1,50 @@
-const baseUrl = localStorage.getItem('baseUrl') || `https://just-share-dont-buy-backend.herokuapp.com`;
+var baseUrl = localStorage.getItem('baseUrl') || `https://just-share-dont-buy-backend.herokuapp.com`;
 
 // Read the item ID from the URL
 let item = $.url('?item');
 
 // When the page loads
 $(document).ready(() => {
+  // Setup up click listener for 'Book' button
+  $('#book').click(event => {
+    axios.patch(`${baseUrl}/items/${item}`, {reserved: true})
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  $('#unbook').click(event => {
+    axios.patch(`${baseUrl}/items/${item}`, {reserved: false})
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  $('#modal').on('hidden.bs.modal', () => {
+    location.reload();
+  });
+
   // Make a GET request for the item
-
   axios.get(`${baseUrl}/items/${item}`)
-
     .then(response => {
-      console.log(response);
       const item = response.data
       $('#item-title').html(item.name)
       $('#item-description').html(item.description)
       $('#item-image').html(`<img src="${item.photo}" height=300 width=425 />`)
       $('#date').html(new Date(item.created_at));
+      if (item.reserved) {
+        $('#book').hide();
+        $('#modal-title').html(`Successfully unbooked ${item.name}!`)
+      } else {
+        $('#unbook').hide();
+        $('#modal-title').html(`Successfully reserved ${item.name}!`)
+      }
       $('#availability').html(item.reserved ? "Not Available" : "Available")
       axios.get(`${baseUrl}/users/${item.user_id}`)
         .then(response => {
@@ -29,21 +58,3 @@ $(document).ready(() => {
       console.log(error);
     });
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById("reservation_button").addEventListener('click', (event) => {
-
-    // $('.alert').alert()
-
-    axios.patch(`${baseUrl}/items/${item}`, {
-    
-        "reserved": true
-      })
-      .then(result => {
-        console.log(result);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  })
-})
