83cf2274a1d6faba694de6959fc50440b507a526
Sophie <sophie4ngo@gmail.com>
2018-04-04 19:56:54 -0700

    Show badges for reserved/unreserved items

--- categories.js
+++ categories.js
@@ -32,21 +32,26 @@ $(document).ready(() => {
       const items = response.data;
       items.forEach(item => {
         let url = window.location.href.split("/category.html?")[0]
-        if (!item.reserved) {
-          $('.card-deck').append(`
-            <div class="card category-item-card mb-4">
-              <a class="a-card" href="${url}/item.html?item=${item.id}">
-
-                <img class="card-img-top" src="${item.photo}" width="286" height="195">
-
-                <div class="card-body">
-                  <h5 class="card-title">${item.name}</h5>
-                  <p class="card-text">${item.description}</p>
-                </div>
-              </a>
-            </div>
-          `);
+        let badge;
+        if (item.reserved) {
+          badge = '<span class="badge badge-secondary float-right mt-1">Unavailable</span>'
+        } else {
+          badge = '<span class="badge badge-success float-right mt-1">Available</span>'
         }
+        $('.card-deck').append(`
+          <div class="card category-item-card mb-4">
+            <a class="a-card" href="${url}/item.html?item=${item.id}">
+
+              <img class="card-img-top" src="${item.photo}" width="286" height="195">
+
+              <div class="card-body">
+                ${badge}
+                <h5 class="card-title">${item.name}</h5>
+                <p class="card-text">${item.description}</p>
+              </div>
+            </a>
+          </div>
+        `);
       })
     })
     .catch(error => {
