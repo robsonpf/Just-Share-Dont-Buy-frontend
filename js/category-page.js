67b646678a6a74aeebd41a07d66b3a0da00a3dc6
Sophie <sophie4ngo@gmail.com>
2018-04-06 01:09:24 -0700

    Fix login/signup

--- js/category-page.js
+++ js/category-page.js
@@ -1,6 +1,10 @@
 import { getCategory, getItemsByCategory } from './app/category.js';
 import { newItem } from './app/item.js';
 
+if (localStorage.getItem('jwtToken')) {
+  axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwtToken');
+}
+
 $(document).ready(() => {
   const category = url('?category');
   getCategory(category);
